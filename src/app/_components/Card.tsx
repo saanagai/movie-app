b611abf3894ed9f9d7a340c70f77b32b5562f723
24LP0542 24LP0542 <24LP0542@G304-CC0227.local>
2025-01-21 12:29:08 +0800

    image

---
+++ src/app/_components/Card.tsx
@@ -0,0 +1,9 @@
+import { MovieType } from "../util/types"
+
+
+export const Card = ((movie):{movie:MovieType})=>{
+    return(
+        <div>Card</div>
+    )
+}
+   
---
+++ src/app/util/types.ts
@@ -0,0 +1,16 @@
+import { MovieType } from "../_components/Card";
+
+export type MovieType = {
+  adult: boolean;
+  backdrop_path: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_data: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
