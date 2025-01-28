c07afdeb6f6789cf807a45b6b7f5c7b91cfcc4a7
Munkhdelger Dembel <saanagai@Munkhdelgers-MacBook-Pro.local>
2025-01-28 22:20:21 +0800

    moreLikeThis

--- src/app/details/[movieId]/page.tsx
+++ src/app/details/[movieId]/page.tsx
@@ -3,6 +3,7 @@ import { createdType } from "@/app/util/createdType";
 import { genreType } from "@/app/util/genreType";
 import { MovieType } from "@/app/util/types";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 export default async function page1({
@@ -28,59 +29,104 @@ export default async function page1({
       },
     }
   );
+  const responseVideos = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-type": "application/json",
+      },
+    }
+  );
+  const similarData = await fetch(
+    ` https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-type": "application/json",
+      },
+    }
+  );
 
-  // function formatVoteAverage(vote: number) {
-  //   const hours = Math.floor(vote / 60);
-  //   const minutes = vote % 60;
-  //   return `${hours}h ${minutes}min`;
-  // }
-
-  // function formatVoteAverage2(vote: number) {
-  //   return (Math.floor(vote * 10) / 10).toString().replace(".", ".");
-  // }
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   const dataStar = await responseStar.json();
-  // console.log(dataStar);
+  console.log(dataStar);
+  const dataVideos = await responseVideos.json();
+  console.log(dataVideos);
+  const dataSimilar = await similarData.json();
+  // console.log(similarData);
   return (
     <div className="max-w-[1280px] m-auto">
       <div>
-        {/* {data.map((movie: MovieType, index: number) => {
-          return <div>{movie.poster_path}</div>;
-        })} */}
-        <h1 className="text-[24px], font-semibold">{data.original_title}</h1>
+        <h1 className="text-[36px] font-bold">{data.original_title}</h1>
         <h2 className="text-[14px]">{data.release_date}</h2>
-        {/* <p>{data.adult ? "R" : "PG"}</p>
-        <p>Rating</p>
-        <p>{formatVoteAverage(data.runtime)}</p>
-
-        <p>{formatVoteAverage2(data.vote_average)}</p> */}
+        {/* <p>{}</p> */}
         <Image
           width={1000}
           height={1000}
           src={"https://image.tmdb.org/t/p/w500" + data.poster_path}
           alt=""
-          className="w-[290px] h-[428px]"
+          className="w-[290px] h-[428px] rounded-sm"
         />
         <div className="flex flex-col  gap-[5px]">
-          <div className="flex">
-            <p>
-              {data.genres.map((genre: genreType, id: number) => {
-                return <div>{genre.name}</div>;
-              })}
-            </p>
-          </div>
+          <p className="flex gap-3">
+            {data.genres.map((genre: genreType, id: number) => {
+              return <div>{genre.name}</div>;
+            })}
+          </p>
+
           <p className="text-[16px]">{data.overview}</p>
           <h2 className="border-b-[1px] text-[16px] font-bold">Director:</h2>
-          <h2 className="border-b-[1px] text-[16px] font-bold">Writers:</h2>
-          <h2 className=" flex border-b-[1px] text-[16px] font-bold">
+          <p className=" flex border-b-[1px] text-[16px] font-bold">
             Stars:
-            {dataStar.cast
-              .slice(0, 5)
-              .map((movie: createdType, index: number) => {
-                return <div>{movie.original_name}</div>;
-              })}
+            {dataStar.cast.slice(0, 1).map((movie: createdType, id: number) => {
+              return <div>{movie.original_name}</div>;
+            })}
+          </p>
+        </div>
+        <div>
+          <h2 className="border-b-[1px] text-[16px] flex gap-16">
+            <p className="text-[16px] w-[64px] font-bold"> Writers</p>
+            {dataStar.crew.slice(0, 1).map((movie: createdType, id: number) => {
+              return <div>{movie.original_name}</div>;
+            })}
           </h2>
+        </div>
+        <div>
+          <h2 className="border-b-[1px] text-[16px]  flex gap-16">
+            <p className="w-[64px] font-bold">Stars</p>
+            {dataStar.cast.slice(0, 5).map((movie: createdType, id: number) => {
+              return <div>·{movie.original_name}</div>;
+            })}
+          </h2>
+        </div>
+        <div className="flex gap-8 mt-20 mb-20">
+          {dataSimilar.results
+            .slice(0, 5)
+            .map((movie: MovieType, id: number) => {
+              return (
+                <Card>
+                  <div>
+                    <div>
+                      <Image
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        width={229.73}
+                        height={340}
+                        alt=""
+                      />
+                    </div>
+                    <div className="p-2">
+                      <div className=" flex items-center">
+                        <p>⭐️ {movie.vote_average.toFixed(1)}</p>
+                        <p className="text-[#71717A] text-[12px]">/10</p>
+                      </div>
+                      <h2 className="text-[18px]">{movie.original_title}</h2>
+                    </div>
+                  </div>
+                </Card>
+              );
+            })}
         </div>
       </div>
     </div>
